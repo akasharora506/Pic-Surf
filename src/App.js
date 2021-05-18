ef2cfc1729f55f63eebd38f53120d1d41ce74fc8
Akash Arora <akasharora@Akashs-MacBook-Air.local>
2021-05-18 21:46:02 +0530

    images are displayed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
     const [searchImage, setSearchImage] = useState("");
-    const [images, setImages] = useState([]);
+    const [images, setImages] = useState({});
 
     useEffect(() => {
         const link = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.REACT_APP_API_KEY}&text=${searchImage}&media=photos&per_page=12&page=1&format=json&nojsoncallback=1`;
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 export default function ImageGallery({ images }) {
+    const { photo } = images;
     return (
         <div>
+            {photo && photo.length ? photo.map((item, index) => {
+                console.log(item);
+                return (<img tabIndex={0} src={`https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg`} alt={'img' + index} />);
+            })
+                : null}
         </div>
     )
 }
